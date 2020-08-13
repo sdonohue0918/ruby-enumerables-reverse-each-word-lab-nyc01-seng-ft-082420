1e02880fb311bdab55664eb874275c01f9539bf2
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-08-13 19:04:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,12 @@
 def reverse_each_word(sentence)
+  return_sentence = []
+
+  mod_sentence = sentence.split(" ")
+
+  mod_sentence.each do |str|
+    return_sentence << str.reverse
+  end
+
+  return_sentence.join(" ")
 
 end
